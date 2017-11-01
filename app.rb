b12a1b58c3328b4f4f9e05126cb55747d6463f59
Ben Orenstein <ben.orenstein@gmail.com>
2017-11-01 10:25:03 -0400

    force ssl

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
+configure :development do
+  set :force_ssl, false
+end
+
+configure :production do
+  set :force_ssl, true
+end
+
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
