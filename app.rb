d6bd86c89a1cc19f260c4bfeba63d4423581db6d
Ben Orenstein <ben.orenstein@gmail.com>
2017-10-31 21:12:30 -0400

    stuff

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
 get '/' do
-  File.read('index.html')
+  File.read(File.join('public', 'index.html'))
+end
+
+get '/thanks' do
+  File.read(File.join('public', 'thanks.html'))
 end
